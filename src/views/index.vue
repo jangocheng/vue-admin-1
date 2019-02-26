387c31f968971f6a6ccb45380cd2682ea301abbd
jry <ijry@qq.com>
2019-02-27 00:58:53 +0800

    首页响应式优化

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,30 @@
 <style scoped lang="less">
+@media screen and (min-width: 768px) {
+    .index-card {
+        height: 350px;
+        &:nth-child(odd) {
+            margin-right: 8px;
+        }
+        &:nth-child(even) {
+            margin-left: 8px;
+        }
+    }
+}
+@media screen and (max-width: 768px) {
+    .index-card {
+        margin-bottom: 15px;
+    }
+}
 </style>
 <template>
     <div class="index">
         <Row type="flex" justify="left" align="top">
-            <Col span="12">
-                <Card :bordered="false" style="margin-right: 8px;height: 350px;">
+            <Col :xs="24" :sm="12">
+                <Card class="index-card" :bordered="false" style="">
                     <p slot="title">系统信息</p>
                     <div>
                         <div>后台名称：InitAdmin后台</div>
+                        <div>官网：https://initadmin.net</div>
                         <div>客户端框架：vue-admin v0.1.0</div>
                         <div>服务端框架：actionphp v0.1.0</div>
                         <div>PHP版本：7.0</div>
@@ -15,8 +32,8 @@
                     </div>
                 </Card>
             </Col>
-            <Col span="12">
-                <Card :bordered="false" style="margin-left: 8px;height: 350px;">
+            <Col :xs="24" :sm="12">
+                <Card class="index-card" :bordered="false">
                     <p slot="title">交流群</p>
                     <div>
                         <img style="width: 200px;margin-right: 10px;" src="@/assets/qun_qq.png">
